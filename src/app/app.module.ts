1e48d1bbd78410df0d508a0e4d24900b78111d72
Digambar Darade <digambardarade@gmail.com>
2025-12-19 00:35:27 +0530

    UI Modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { SimplePuzzleComponent } from './simple-puzzle/simple-puzzle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SimplePuzzleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
